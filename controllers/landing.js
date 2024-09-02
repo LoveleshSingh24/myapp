788bfc989bc5a69cd79756605fbd14c78e94a424
lovelesh24 <lovesinghsisodiya2403@gamil.com>
2024-09-03 05:13:43 +0530

    Intialize Sequelize in bin/www

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,10 +1,13 @@
-
+const models = require('../models')
 module.exports = {
     get_landing: function(req, res, next) {
         res.render('landing', { title: 'Express' });
     },
     submit_lead: function(req, res, next) {
-        console.log("lead email :" , req.body.lead_email);
-        res.redirect('/');
+        return models.Lead.Create({
+            email:req.body.lead_email
+        }).then(lead => {
+            res.redirect('/')
+        })
     }
 };
