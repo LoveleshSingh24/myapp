06d20645c29d7c5fe5bf50d376248c36be69ee3b
lovelesh24 <lovesinghsisodiya2403@gamil.com>
2024-09-06 19:19:47 +0530

    Added new route to display lead details

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,13 +1,47 @@
-const models = require('../models')
+const models = require('../models');
+
 module.exports = {
     get_landing: function(req, res, next) {
         res.render('landing', { title: 'Express' });
     },
     submit_lead: function(req, res, next) {
         return models.Lead.create({
-            email:req.body.lead_email
-        }).then(lead => {
-            res.redirect('/')
+            email: req.body.lead_email
         })
-    }
+        .then(lead => {
+            res.redirect('/leads');
+        })
+        .catch(error => {
+            console.error("Error creating lead:", error);
+            res.status(500).send("An error occurred while submitting the lead.");
+        });
+    },
+    show_leads: function(req, res, next) {
+        return models.Lead.findAll()
+        .then(leads => {
+            res.render('landing', { title: 'Express', leads: leads });
+        })
+        .catch(error => {
+            console.error("Error retrieving leads:", error);
+            res.status(500).send("An error occurred while retrieving the leads.");
+        });
+    },
+    show_lead: function(req, res, next) {
+        return models.Lead.findOne({
+            where: {
+                id: req.params.lead_id
+            }
+        })
+        .then(lead => {
+            if (lead) {
+                res.render('lead', { lead: lead });
+            } else {
+                res.status(404).send("Lead not found.");
+            }
+        })
+        .catch(error => {
+            console.error("Error retrieving lead:", error);
+            res.status(500).send("An error occurred while retrieving the lead.");
+        });
+    }    
 };
